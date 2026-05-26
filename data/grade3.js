068d758feab33627df1aa6e55952357d0d2e27a8
Tina-Nick <furuta@chuman.co.jp>
2026-05-27 00:42:19 +0900

    Update grade3.js

--- data/grade3.js
+++ data/grade3.js
@@ -1,1 +1,27 @@
-const grade3=[]
+const grade3=[
+
+{
+id:46001,
+
+grade:3,
+
+unit:46,
+
+title:"現在完了",
+
+japanese:
+"私は１度京都を訪れたことがあります。",
+
+english:
+"I have visited Kyoto once.",
+
+chunks:[
+"I",
+"have visited",
+"Kyoto",
+"once"
+]
+
+}
+
+]
