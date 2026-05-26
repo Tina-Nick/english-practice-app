1d842d45ae64d1cff3855a5d569d5075e3d57c26
Tina-Nick <furuta@chuman.co.jp>
2026-05-27 01:12:27 +0900

    Update grade1.js

--- data/grade1.js
+++ data/grade1.js
@@ -1,2 +1,113 @@
 const grade1 = [
+
+{
+id:10001,
+grade:1,
+unit:1,
+title:"be動詞",
+
+japanese:"私はケンです。",
+
+english:"I am Ken.",
+
+chunks:[
+"I",
+"am",
+"Ken"
+]
+
+},
+
+{
+id:10002,
+grade:1,
+unit:1,
+title:"be動詞",
+
+japanese:"彼女は私の友達です。",
+
+english:"She is my friend.",
+
+chunks:[
+"She",
+"is",
+"my friend"
+]
+
+},
+
+{
+id:10003,
+grade:1,
+unit:1,
+title:"be動詞",
+
+japanese:"あなたは忙しいですか。",
+
+english:"Are you busy?",
+
+chunks:[
+"Are",
+"you",
+"busy"
+]
+
+},
+
+{
+id:10004,
+grade:1,
+unit:2,
+title:"一般動詞",
+
+japanese:"私は毎日サッカーをします。",
+
+english:"I play soccer every day.",
+
+chunks:[
+"I",
+"play",
+"soccer",
+"every day"
+]
+
+},
+
+{
+id:10005,
+grade:1,
+unit:2,
+title:"一般動詞",
+
+japanese:"彼は英語を勉強します。",
+
+english:"He studies English.",
+
+chunks:[
+"He",
+"studies",
+"English"
+]
+
+},
+
+{
+id:10006,
+grade:1,
+unit:2,
+title:"一般動詞",
+
+japanese:"あなたはピアノを弾きますか。",
+
+english:"Do you play the piano?",
+
+chunks:[
+"Do",
+"you",
+"play",
+"the piano"
+]
+
+}
+
 ];
