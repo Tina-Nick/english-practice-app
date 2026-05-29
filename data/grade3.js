edc0c72e1a24fd8e8a0999cae48eaf66b80a50c2
Tina-Nick <furuta@chuman.co.jp>
2026-05-29 23:11:28 +0900

    Update grade3.js

--- data/grade3.js
+++ data/grade3.js
@@ -17,9 +17,122 @@ english:
 
 chunks:[
 "I",
-"have visited",
+"have",
+"visited",
 "Kyoto",
 "once"
+]
+
+},
+{
+id:30002,
+grade:3,
+unit:31,
+title:"現在完了",
+
+japanese:
+"私はちょうど宿題を終えたところです。",
+
+english:
+"I have just finished my homework.",
+
+chunks:[
+"I",
+"have",
+"just",
+"finished",
+"my homework"
+]
+
+},
+
+{
+id:30003,
+grade:3,
+unit:31,
+title:"現在完了",
+
+japanese:
+"彼は3年間ずっと名古屋に住んでいます。",
+
+english:
+"He has lived in Nagoya for three years.",
+
+chunks:[
+"He",
+"has",
+"lived",
+"in",
+"Nagoya",
+"for",
+"three years"
+]
+
+},
+
+{
+id:30004,
+grade:3,
+unit:31,
+title:"現在完了",
+
+japanese:
+"あなたは今までに京都を訪れたことがありますか。",
+
+english:
+"Have you ever visited Kyoto?",
+
+chunks:[
+"Have",
+"you",
+"ever",
+"visited",
+"Kyoto"
+]
+
+},
+
+{
+id:30005,
+grade:3,
+unit:31,
+title:"現在完了",
+
+japanese:
+"私はまだ昼食を食べていません。",
+
+english:
+"I have not eaten lunch yet.",
+
+chunks:[
+"I",
+"have",
+"not",
+"eaten",
+"lunch",
+"yet"
+]
+
+},
+
+{
+id:30006,
+grade:3,
+unit:31,
+title:"現在完了",
+
+japanese:
+"彼女はすでにその本を読んでしまいました。",
+
+english:
+"She has already read the book.",
+
+chunks:[
+"She",
+"has",
+"already",
+"read",
+"the book"
 ]
 
 }
