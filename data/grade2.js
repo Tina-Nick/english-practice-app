c847025f873314366d3c90747b4a82ead2bd0fa5
Tina-Nick <furuta@chuman.co.jp>
2026-05-29 18:22:32 +0900

    Update grade2.js

--- data/grade2.js
+++ data/grade2.js
@@ -1,2 +1,93 @@
 const grade2 = [
+
+{
+id:20001,
+grade:2,
+unit:21,
+title:"比較",
+
+japanese:
+"私の犬はあなたの犬より大きいです。",
+
+english:
+"My dog is bigger than yours.",
+
+chunks:[
+"My dog",
+"is",
+"bigger",
+"than",
+"yours"
+]
+
+},
+
+{
+id:20002,
+grade:2,
+unit:21,
+title:"比較",
+
+japanese:
+"この本はあの本より面白いです。",
+
+english:
+"This book is more interesting than that one.",
+
+chunks:[
+"This book",
+"is",
+"more",
+"interesting",
+"than",
+"that one"
+]
+
+},
+
+{
+id:20003,
+grade:2,
+unit:21,
+title:"比較",
+
+japanese:
+"彼はクラスで最も速く走ります。",
+
+english:
+"He runs the fastest in his class.",
+
+chunks:[
+"He",
+"runs",
+"the fastest",
+"in",
+"his class"
+]
+
+},
+
+{
+id:20004,
+grade:2,
+unit:21,
+title:"比較",
+
+japanese:
+"私は数学より英語のほうが好きです。",
+
+english:
+"I like English better than math.",
+
+chunks:[
+"I",
+"like",
+"English",
+"better",
+"than",
+"math"
+]
+
+}
+
 ];
